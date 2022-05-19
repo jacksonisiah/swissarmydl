5cbc754542e33a394533e7965083835c0fa9f14f
Jackson <jacksonisiah01@gmail.com>
2022-05-19 07:03:00 -0400

    fix settings menu

--- src/app/settings-dialog/settings-dialog.component.ts
+++ src/app/settings-dialog/settings-dialog.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
   templateUrl: './settings-dialog.component.html',
   styleUrls: ['./settings-dialog.component.scss']
 })
+
 export class SettingsDialogComponent implements OnInit {
 
   constructor(public dialog: MatDialog) {}
